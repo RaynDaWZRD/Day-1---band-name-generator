3f7a27b189ff1e7ad3fb5dc0d95f9ac1cd04c33b
Ryan Lopez <123214627+RaynDaWZRD@users.noreply.github.com>
2023-01-25 17:12:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello there! Lets get you a band name...")
+
+city = input("What city are you from?\n")
+
+pet_name = input("What is your pet's name?\n")
+
+print("Your band name can be " + city + " " + pet_name + "!")
